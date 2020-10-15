b2d3c54baa2efa0e4a5390ab51f6f595b83f10f3
Jeremy Iverson <jiverson002@csbsju.edu>
2020-10-15 14:14:26 -0500

    Initial commit

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+/* hash algorithm, as presented in K&R version 2 */
+unsigned hash(const char *s) {
+  unsigned hashval;
+  for (hashval = 0; *s != '\0'; s++)
+    hashval = *s + 31 * hashval;
+  return hashval;
+}
+
+/* program entry point */
+int main(int argc, char *argv[]) {
+  const char * user = getenv("USER");
+  printf("\
+    hello, %s\n\
+    To complete this assignment, please submit the following code using the\n\
+    textbox submission on Canvas. Your code is:\n\n\
+    %u\n\
+  ", user, hash(user));
+}
